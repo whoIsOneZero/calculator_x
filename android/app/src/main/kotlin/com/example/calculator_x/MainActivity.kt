385b7ab7fff5f675b5ca34934fcae892a991f15f
whoIsOneZero <oh.shalom.0@gmail.com>
2023-05-08 10:35:28 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/calculator_x/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calculator_x
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/calculator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: const Text('CalculatorX'), backgroundColor: Colors.black,),
+    );
+  }
+}
