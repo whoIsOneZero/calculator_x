906824e6675fb5b920fbf4d4acc1fb70bacf7cf5
whoIsOneZero <oh.shalom.0@gmail.com>
2023-05-26 14:31:58 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import '../widgets/calculator.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Calculator(),
     );
